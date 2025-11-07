04d6b1a6f6a45a7d37f6b08004181ee78be12691
volkan4513 <106133149+volkan4513@users.noreply.github.com>
2025-11-07 12:41:40 +0530

    upodate

---
+++ playlist.js
@@ -0,0 +1,21 @@
+
+const hardcodedMusicData = [
+    {
+        "filename": "./music/_Sunstroke_Project_Olia_Tira_._-_Run_Away_-_(mp3.pm).mp3",
+        "title": "Run_Away",
+        "artist": "Sunstroke",
+        "album": ""
+    },
+    {
+        "filename": "./music/10. Twinleaf Town (Day).mp3",
+        "title": "Twinleaf Town (Daytime)",
+        "artist": "Hitomi Sato",
+        "album": "Pokémon Diamond, Pearl, & Platinum: Original Soundtrack"
+    },
+    {
+        "filename": "./music/kpasi-music_naruto-shippuden-opening-03-blue-bird.mp3",
+        "title": "Naruto Shippuden Opening 03 - Blue Bird",
+        "artist": "KPASI Music",
+        "album": ""
+    }
+];
